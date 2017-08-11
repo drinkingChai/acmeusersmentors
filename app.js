13955d6f22e764df6049be365bd3829f7b3297c5
Wasif Zaman <teatocode@gmail.com>
2017-08-11 14:24:03 -0400

    Set up express app

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const nunjucks = require('nunjucks');
+const	users = require('./routes');
+const conn = require('./models');
+
+const app = express();
+
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(users);
+
+
+const port = process.env.PORT || 3000;
+
+conn.sync()
+	.then(()=> {
+		app.listen(port, ()=> {
+			console.log(`listening on port ${port}`);
+		})
+	})
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const conn = require('../models');
+
+
+module.exports = router;
