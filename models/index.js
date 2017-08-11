1e900ac81ee11207d9a425ad5d8e25c45fb92367
Wasif Zaman <teatocode@gmail.com>
2017-08-11 17:38:35 -0400

    saving

--- models/index.js
+++ models/index.js
@@ -2,8 +2,37 @@ const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL);
 
 
-const Page = conn.define('page', {
+const User = conn.define('user', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	isMentor: {
+		type: Sequelize.BOOLEAN,
+		default: false,
+		allowNull: false
+	},
+	awards: {
+		type: Sequelize.ARRAY(Sequelize.INTEGER),
+		default: []
+	}
+})
 
+User.prototype.addAward = function(phrase) {
+	this.setDataValue('awards', this.getDataValue('awards').push(phrase));
+};
+
+User.prototype.promote = function() {
+	if (this.getDataValue('awards').length >= 2) {
+		this.setDataValue('isMentor', true);
+	} else throw new Error('Not enough awards');
+}
+
+const Award = conn.define('award', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
 })
 
 
@@ -16,6 +45,7 @@ const sync = ()=> {
 module.exports = {
 	sync,
 	models: {
-		Page
+		User,
+		Award
 	}
 };
